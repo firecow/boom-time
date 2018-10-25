dc6dd2de46313ddfc75651a3a14e17d7af5254ca
Mads Jon Nielsen <madsjon@gmail.com>
2018-10-25 18:53:12 +0200

    Parse and show gold

--- php/src/Routes/ShowCharacterProfiler.php
+++ php/src/Routes/ShowCharacterProfiler.php
@@ -30,10 +30,23 @@ class ShowCharacterProfiler extends Route
         if (!file_exists($filePath)) {
             throw new NotFoundRouteException("(404) Cannot find '$charName'");
         }
-        $itemMap = $this->parseToItemMap($filePath);
+
+        $luaParser = new LUAParser();
+        $luaParser->parseFile($filePath);
+        $strp = explode('-', str_replace("../data/character_profiler/", "", $filePath));
+
+        $realm = str_replace("_", " ", $strp[0]);
+        $charName = str_replace(".lua", "", $strp[1]);
+
+        $objectPath = new ObjectPath($luaParser->data);
+        $itemMap = $this->parseToItemMap($objectPath, $realm, $charName);
+
+        $ob = $objectPath->get("myProfile[$realm][$charName][Money][Gold]");
+        $gold = $ob ? $ob->getPropertyValue() : 0;
         $data = [
             "title" => $charName,
-            "itemMap" => $itemMap
+            "itemMap" => $itemMap,
+            "gold" => $gold,
         ];
         $phtml = PHTML::create("src/Routes/ShowCharacterProfiler.phtml", $data, $ctx);
         return new HtmlTextRouteResponse($phtml);
@@ -47,18 +60,7 @@ class ShowCharacterProfiler extends Route
         return [];
     }
 
-    private function parseToItemMap(string $filePath): array {
-        $luaParser = new LUAParser();
-        $luaParser->parseFile($filePath);
-        $strp = explode('-', str_replace("../data/character_profiler/", "", $filePath));
-
-        $realm = str_replace("_", " ", $strp[0]);
-        $charName = str_replace(".lua", "", $strp[1]);
-
-        //echo "$realm\n";
-        //echo "$charName\n";
-
-        $objectPath = new ObjectPath($luaParser->data);
+    private function parseToItemMap(ObjectPath $objectPath, string $realm, string $charName): array {
 
         $items = array_merge(
             $this->getValueOrEmptyArray($objectPath, "myProfile[$realm][$charName][Bank][Contents]"),
@@ -80,6 +82,9 @@ class ShowCharacterProfiler extends Route
         $items = array_map(function($item) {
             $item["Quantity"] = (int)$item["Quantity"];
             $item["Color"] = "#" . substr($item["Color"], 2);
+            $item["Id"] = $item["Item"];
+            $colonPos = strpos($item["Id"], ":");
+            $item["Id"] = substr($item["Id"], 0, $colonPos);
             $item["Texture"] = strtolower(str_replace("Interface\\\\Icons\\\\", "", $item["Texture"]));
             return $item;
         }, $items);
