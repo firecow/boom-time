77ea7e56d72db8b34d4768f1b9cc71a36c5e9ae8
Mads Jon Nielsen <madsjon@gmail.com>
2018-11-08 22:34:29 +0100

    Update OfficerNotes for characters

--- php/src/Routes/UploadCharacterProfilerLua.php
+++ php/src/Routes/UploadCharacterProfilerLua.php
@@ -5,6 +5,8 @@ namespace App\Routes;
 
 use App\Context;
 use App\Exceptions\UnprocessableRouteException;
+use App\LUAParser;
+use App\ObjectPath\ObjectPath;
 use App\Responses\JsonRouteResponse;
 use App\Responses\RouteResponse;
 use App\Router\Route;
@@ -27,6 +29,7 @@ class UploadCharacterProfilerLua extends Route
         $jwt = $ctx->getAccessToken();
         $payload = $jwtHandler->decode($jwt);
         $realm = $payload["realm"];
+        $sql = $ctx->createSQL();
 
         $filesData = $ctx->getSuperGlobals()->getHTTPFilesData("file");
         if ($filesData["error"] !== 0) {
@@ -34,6 +37,21 @@ class UploadCharacterProfilerLua extends Route
         }
 
         file_put_contents("../data/character_profiler/$realm"."-"."$charName.lua", file_get_contents($filesData["tmp_name"]));
+
+        $luaParser = new LUAParser();
+        $luaParser->parseFile("../data/character_profiler/$realm"."-"."$charName.lua");
+
+        $objectPath = new ObjectPath($luaParser->data);
+        $obValue = $objectPath->get("myProfile[Kronos III][Guild][Members]");
+        if ($obValue) {
+            $members = $obValue->getPropertyValue();
+            foreach ($members as $member) {
+                $charName = $member["Name"];
+                $officerNote = $member["OfficerNote"];
+                $sql->execute("UPDATE characters SET officerNote = ? WHERE charName = ?", [$officerNote, $charName]);
+            }
+        }
+
         return new JsonRouteResponse($filesData);
     }
 }
